ff7edf2440445a65d2a5ef6998a6eaecee16520f
Jason Gionfriddo <Ragtop66goat@gmail.com>
2022-11-18 22:59:52 -0600

    added titleRef and contentRef vars to ThreadInput.js moved dispatch calls for username and password into register/login btn functions. added useRef to persist user input

--- src/components/ThreadInput/ThreadInput.js
+++ src/components/ThreadInput/ThreadInput.js
@@ -1,6 +1,7 @@
 import {useSelector, useDispatch} from "react-redux";
 import {v4 as uuidv4} from 'uuid';
 import {ON_SUBMIT_THREAD, ON_SET_ID, ON_SET_THREAD_CONTENT, ON_SET_THREAD_TITLE} from "../../modules/threads";
+import {useRef} from "react";
 
 export function ThreadInput(props) {
 
@@ -8,52 +9,45 @@ export function ThreadInput(props) {
     _useSelector = useSelector,
     _useDispatch = useDispatch,
     _uuidv4 = uuidv4
-  } = props
+  } = props;
 
-  const threadTitle = _useSelector((state) => state.threadTitle)
-  const threadContent = _useSelector((state) => state.threadContent)
-  const threadId = _useSelector((state)=> state.id)
-  const dispatch = _useDispatch()
-
-  //todo: implement useRef for title and content input
-  function onTitleChange(e) {
-    dispatch({type: ON_SET_THREAD_TITLE, value: e.target.value})
-  }
-
-  function onContentChange(e) {
-  dispatch({type: ON_SET_THREAD_CONTENT, value: e.target.value})
-  }
+  const threadId = _useSelector((state) => state.id);
+  const dispatch = _useDispatch();
+  const titleRef = useRef();
+  const contentRef = useRef();
 
   function submitThread() {
     let id;
     // assigns new Id if it is not an edit
-    if(!threadId){
+    if (!threadId) {
       id = _uuidv4();
-    } else{
-      id = threadId
+    } else {
+      id = threadId;
     }
-    dispatch({type: ON_SET_ID, value: id})
-    const date = new Date().toString().substring(0, 15)
-    dispatch({type: ON_SUBMIT_THREAD, value: date})
+    dispatch({type: ON_SET_THREAD_TITLE, value: titleRef.current.value})
+    dispatch({type: ON_SET_THREAD_CONTENT, value: contentRef.current.value})
+    dispatch({type: ON_SET_ID, value: id});
+    const date = new Date().toString().substring(0, 15);
+    dispatch({type: ON_SUBMIT_THREAD, value: date});
   }
 
   //conditional styling/text elements
-  const setHeader = threadId ? "Edit Thread" : "Post Thread?"
-  const setBtnColor = threadId ? "info" : "success"
-  const setBG = threadId ? "light" : "white"
-  const setBTN = threadId ? "Submit Edit" : "Submit"
+  const setHeader = threadId ? "Edit Thread" : "Post Thread?";
+  const setBtnColor = threadId ? "info" : "success";
+  const setBG = threadId ? "light" : "white";
+  const setBTN = threadId ? "Submit Edit" : "Submit";
 
 
-return <>
-  <div>
-    <h1 className={"text-center bg-dark bg-opacity-75 text-white border rounded p-2"}>Threadboard</h1>
-  </div>
-  <div className={`d-flex flex-column shadow p-3 mb-5 bg-${setBG} rounded p-2`}>
-    <h4>{setHeader}</h4>
-    <input onChange={onTitleChange} value={threadTitle} type={'text'} placeholder={'Title'}/>
-    <input onChange={onContentChange} value={threadContent} className={'mt-3'} type={'text'} placeholder={'content'}/>
-    <button className={`btn-sm btn-${setBtnColor} w-25 mt-2`} onClick={submitThread}>{setBTN}</button>
-  </div>
+  return <>
+    <div>
+      <h1 className={"text-center bg-dark bg-opacity-75 text-white border rounded p-2"}>Threadboard</h1>
+    </div>
+    <div className={`d-flex flex-column shadow p-3 mb-5 bg-${setBG} rounded p-2`}>
+      <h4>{setHeader}</h4>
+      <input ref={titleRef} type={'text'} placeholder={'Title'}/>
+      <input ref={contentRef} type={'text'} className={'mt-3'} placeholder={'content'}/>
+      <button className={`btn-sm btn-${setBtnColor} w-25 mt-2`} onClick={submitThread}>{setBTN}</button>
+    </div>
 
-</>
+  </>;
 }
